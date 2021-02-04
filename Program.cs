eda8de7f3beef694002af2e9eade92c53c7f2b3d
Kevin Sigmund <ksigmund@microsoft.com>
2021-02-04 13:54:24 -0800

    fixup

--- Program.cs
+++ Program.cs
@@ -21,6 +21,9 @@ namespace kanji
                 new[] { 0, 1, 0, 1, 0 },
                 new[] { 1, 0, 0, 0, 1, 0, 0 },
                 new[] { 1, 0, 0, 0, 1, 1, 0 },
+                new[] { 1, 0, 0, 0, 1, 1, 0, 0 },
+                new[] { 0, 0, 0, 0, 1, 1, 0 },
+                new[] { 1, 0, 1, 0, 1, 1, 0 },
                 new[] { 1, 0 },
                 new[] { 0, 1 },
                 new[] { 1 },
@@ -43,16 +46,17 @@ namespace kanji
         /// <returns>The number of backspaces required in order to delete the last character (be it Kanji or ASCII)</returns>
         public static int CalculateBackspaces(int[] bytes) => bytes
                 .Reverse()
-                .Aggregate(0, (backspaces, val) => // Reverse + Aggregate is a functional right fold
+                .Aggregate((Backspaces: 0, Done: false), (state, currentVal) => // Reverse + Aggregate is a functional right fold
                 {
-                    switch (backspaces, val) // evaluate the # of backspaces calculated so far and the current value
+                    switch (state.Done, state.Backspaces, currentVal) // evaluate the # of backspaces calculated so far and the current value
                     {
-                        case (0, _): return backspaces + 1;                     // no backspaces yet
-                        case (int b, 1) when b == 2: return backspaces - 1;     // 2 backspaces and encountered a 1
-                        case (int b, 1) when b < 2: return backspaces + 1;      // < 2 backspaces and encountered a 1
-                        case (_, 0): return backspaces;                         // encountered a 0, but already have calculated some backspaces
+                        case (true, _, _): return state;                       // we are already done, return the current state until the end is reached 
+                        case (_, 0, _):                                        // no backspaces calculated so far, assume 1 (at least) must be returned and continue 
+                        case (_, 2, 1): return (1, false);                     // found a 1, but have already calculated 2 backspaces; decrease the count and continue    
+                        case (_, int b, 0): return (b, true);                  // found a 0 and have already calculated a non-zero number of backspaces (we are done)
+                        case (_, int b, 1) when b < 2: return (b+1, false);    // found a 1 and have calculated less than 2 backspaces; increase the count and continue
                         default: throw new Exception("This condition shouldn't occur in the context of this problem.");
                     }
-                });
+                }, x => x.Backspaces);
     }
 }
